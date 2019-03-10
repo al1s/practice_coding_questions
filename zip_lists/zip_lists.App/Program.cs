0022367306da1652bda2b1ae239375d94774dc76
alstof <alstof@gmail.com>
2019-03-09 16:34:56 -0800

    Initial commit with first task

---
+++ zip_lists/zip_lists.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zip_lists.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
